f8648c48d881b4ae29919f27785088fe7478e9aa
Allysson Santos <aallysson0@gmail.com>
2020-05-15 16:04:32 -0300

    refactor(explorer): remove unecessary elements from Layout component

--- packages/explorer/src/components/Layout.jsx
+++ packages/explorer/src/components/Layout.jsx
@@ -5,20 +5,7 @@ import { ThemeProvider, GlobalStyle } from '@react95/core';
 const Layout = ({ children }) => (
   <ThemeProvider>
     <GlobalStyle />
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0 1.0875rem 1.45rem`,
-      }}
-    >
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
-    </div>
+    <main>{children}</main>
   </ThemeProvider>
 );
 
