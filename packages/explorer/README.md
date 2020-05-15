8768957b6ca10f4dae0d4f4d80c4518fd505358a
Allysson Santos <aallysson0@gmail.com>
2020-05-15 11:15:36 -0300

    feat(explorer): init gatsby project with default configuration
    re #33

---
+++ packages/explorer/README.md
@@ -0,0 +1,5 @@
+![React95 Logo](packages/core/components/shared/assets/React95-clouds.png)
+
+# Explorer
+
+Explorer is the documentation website for the React95 library.
---
+++ packages/explorer/gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: `React95`,
+    description: `A component library based on Windows 95. Made with React and styled-components`,
+    author: '@React95',
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `React95/explorer`,
+        short_name: `Explorer`,
+        start_url: `/`,
+        background_color: `#000e7a`,
+        theme_color: `#000e7a`,
+        display: `minimal-ui`,
+        icon: `src/images/react95.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-mdx`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+};
